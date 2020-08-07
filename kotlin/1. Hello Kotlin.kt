48f2eccee2f2826352a68200068268f2cce14dee
3103119100@student.smktelkom-pwt.sch.id <3103119100@student.smktelkom-pwt.sch.id>
2020-08-07 09:28:15 +0700

    first commit

---
+++ kotlin/1. Hello Kotlin.kt
@@ -0,0 +1,10 @@
+fun main () {
+    val name = "Labibah"
+    val hobi = "Membaca Novel"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true) "always true" else "always false")
+}
---
+++ kotlin/2. Data Types.kt
@@ -0,0 +1,11 @@
+fun main (){
+    val firstWord = "SMK "
+    val middleWord = "Telkom "
+    val lastWord = "Purwokerto "
+    println(firstWord + middleWord + lastWord)
+
+    val valueA= 43
+    val valueB = 40
+    val valueC = valueA + valueB
+    println("Luas persegi panjang adalah " + valueC)
+}
